269ee2414740dfca7c5115a08cce713f6b0b9b22
DmsJin <xing_jin@berkeley.edu>
2026-02-18 08:45:13 +0000

    Revert webshop env to RAGEN2 alignment: simpler render, detailed instruction, raw rewards, add raw_reward metric

--- train_webshop_low_filter25.sh
+++ train_webshop_low_filter25.sh
@@ -4,16 +4,22 @@ MKL_SERVICE_FORCE_INTEL=1 python train.py --config-name _6_webshop \
     algorithm.adv_estimator=grpo \
     $USE_LIMITED_MULTI_TURN \
     trainer.project_name="main_webshop" \
-    trainer.experiment_name=webshop3b_nofilter_wndw3_grpo \
+    trainer.experiment_name=webshop3b_top_p90_wndw3_grpo_revert_alignment \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
-    es_manager.train.env_groups=16 es_manager.train.group_size=8 es_manager.train.env_configs.n_groups=[16] \
-    es_manager.val.env_configs.n_groups=[32] \
-    es_manager.val.env_groups=32 \
-    es_manager.val.group_size=16 \
+    es_manager.train.env_groups=4 es_manager.train.group_size=8 es_manager.train.env_configs.n_groups=[4] \
+    es_manager.val.env_configs.n_groups=[2] \
+    es_manager.val.env_groups=2 \
+    es_manager.val.group_size=4 \
     system.CUDA_VISIBLE_DEVICES=\"0,1\" trainer.n_gpus_per_node=2 \
-    trainer.default_local_dir=/mnt/permanent/xjin/20260118/webshop3b_nofilter_wndw3_grpo \
+    trainer.default_local_dir=/mnt/permanent/xjin/20260118/webshop3b_top_p90_wndw3_grpo_revert_alignment \
     trainer.nnodes=1 micro_batch_size_per_gpu=1 \
     ppo_mini_batch_size=64 \
     agent_proxy.max_turn=15 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.rollout_filter_strategy=top_p \
+    actor_rollout_ref.rollout.rollout_filter_value=0.9 \
+    actor_rollout_ref.rollout.rollout_filter_include_zero=true \
+    actor_rollout_ref.rollout.rollout_filter_type=largest \
+    actor_rollout_ref.rollout.rollout_filter_metric=reward_variance \
+    collapse_detection.first_turn_enabled=False \
     collapse_detection.multi_turn_enabled=False
